a7a4899723e8bc3f92d4be47f58ef53520405592
Steven He <hez2010@outlook.com>
2022-03-06 22:30:28 +0800

    Minor simplification

--- Expr.TernaryExpr.cs
+++ Expr.TernaryExpr.cs
@@ -13,7 +13,7 @@ public abstract partial class Expr<T> where T : IBinaryNumber<T>
 
         public override T Eval(params (string Name, T Value)[] args) => Cond.Eval(args) == T.Zero ? Right.Eval(args) : Left.Eval(args);
         public override string ToString() => $"({Cond}) ? ({Left}) : ({Right})";
-        public override bool Equals(object? obj) => obj is TernaryExpr(var cond, var left, var right) && cond.Equals(Cond) && left.Equals(Left) && right.Equals(Right);
+        public override bool Equals(object? obj) => obj is TernaryExpr(var cond, var left, var right) && (cond, left, right).Equals((Cond, Left, Right));
         public override int GetHashCode() => (Cond, Left, Right).GetHashCode();
     }
 }
